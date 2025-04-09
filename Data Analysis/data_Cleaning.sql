e054cff8e691bbc1406c09a1761f19c193b76f96
martin-anju <anjupalakeel@gmail.com>
2025-04-09 17:35:24 +1000

    Add new data_Cleaning.sql file for data cleaning

---
+++ Data Analysis/data_Cleaning.sql
@@ -0,0 +1,43 @@
+-- Remove Duplicates
+CREATE TABLE cleaned_coles_data AS
+SELECT DISTINCT *
+FROM coles_data;
+
+CREATE TABLE cleaned_woolies_data AS
+SELECT DISTINCT *
+FROM woolies_data;
+
+-- Filter rows based on category (for Coles)
+DELETE FROM cleaned_coles_data
+WHERE category = 'Bonus Ovenware Credits';
+
+-- Filter rows based on category (for Woolies)
+DELETE FROM cleaned_woolies_data
+WHERE category = 'Electronics' OR category ='Home & Lifestyle';
+
+-- Convert price columns to numeric (remove non-numeric characters)
+UPDATE cleaned_coles_data
+SET best_price = CAST(REGEXP_REPLACE(best_price::TEXT, '[^\d.]', '', 'g') AS DECIMAL(10, 2)),
+    item_price = CAST(REGEXP_REPLACE(item_price::TEXT, '[^\d.]', '', 'g') AS DECIMAL(10, 2)),
+    best_unit_price = CAST(REGEXP_REPLACE(best_unit_price::TEXT, '[^\d.]', '', 'g') AS DECIMAL(10, 2)),
+    unit_price = CAST(REGEXP_REPLACE(unit_price::TEXT, '[^\d.]', '', 'g') AS DECIMAL(10, 2));
+
+UPDATE cleaned_woolies_data
+SET best_price = CAST(REGEXP_REPLACE(best_price::TEXT, '[^\d.]', '', 'g') AS DECIMAL(10, 2)),
+    item_price = CAST(REGEXP_REPLACE(item_price::TEXT, '[^\d.]', '', 'g') AS DECIMAL(10, 2)),
+    best_unit_price = CAST(REGEXP_REPLACE(best_unit_price::TEXT, '[^\d.]', '', 'g') AS DECIMAL(10, 2)),
+    unit_price = CAST(REGEXP_REPLACE(unit_price::TEXT, '[^\d.]', '', 'g') AS DECIMAL(10, 2));
+
+-- Remove rows with missing values in key columns
+DELETE FROM cleaned_coles_data
+WHERE best_price IS NULL
+   OR item_price IS NULL
+   OR best_unit_price IS NULL
+   OR unit_price IS NULL;
+
+DELETE FROM cleaned_woolies_data
+WHERE best_price IS NULL
+   OR item_price IS NULL
+   OR best_unit_price IS NULL
+   OR unit_price IS NULL;
+
